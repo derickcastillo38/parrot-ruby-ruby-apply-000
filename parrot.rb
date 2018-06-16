ed0bb6adead4198253df27f72e48d581bcaf905e
derickcastillo38 <9derick@gmail.com>
2018-06-16 18:21:09 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(arg = "Squawk!")
-  puts arg
+def parrot(arg)
+  if arg.nil?
+    puts "Squawk"
+  else 
+    puts arg 
+  end 
 end
